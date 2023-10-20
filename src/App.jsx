9b99fbe4ffd744ea0151640bb142174014d9b995
TaiefSiddique <taiefsiddik2@gmail.com>
2023-10-20 21:47:38 +0600

    insertion functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
 
 function App() {
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user)
+    fetch('http://localhost:5000/users', {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }
 
   return (
     <>
       <h1>Simple Crud</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   )
 }
